37b0039f628e582ed14be9db958d6077f03c36f6
ShoUshikubo <sh-ushikubo@kddi.com>
2018-07-12 08:55:06 +0900

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_path(self):
+        self.assertEqual(8, 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
